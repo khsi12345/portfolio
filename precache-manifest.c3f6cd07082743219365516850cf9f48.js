5fcb5de35abc7b1710f4ce26f754e9578037cdfb
khsi12345 <khsi6237@gmail.com>
2020-01-17 00:11:55 +0900

    Updates

---
+++ precache-manifest.c3f6cd07082743219365516850cf9f48.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17934a4cecb1b08da0136f16057fad3f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "335b515f2a02d26975d4",
+    "url": "/portfolio/static/css/main.e0685be0.chunk.css"
+  },
+  {
+    "revision": "096707be3d8d8ff0db7b",
+    "url": "/portfolio/static/js/2.fe4a8a04.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio/static/js/2.fe4a8a04.chunk.js.LICENSE"
+  },
+  {
+    "revision": "335b515f2a02d26975d4",
+    "url": "/portfolio/static/js/main.ada87360.chunk.js"
+  },
+  {
+    "revision": "b08d4cff5182d0bcfccc",
+    "url": "/portfolio/static/js/runtime-main.0c115fd9.js"
+  },
+  {
+    "revision": "2074aac8386f0681d46f47cff9148226",
+    "url": "/portfolio/static/media/bnb메인.2074aac8.png"
+  },
+  {
+    "revision": "9844bdbbe3de7732e1d85713d6bf2757",
+    "url": "/portfolio/static/media/team_dash.9844bdbb.png"
+  },
+  {
+    "revision": "7fd1a7a72b0559ee193560b66be3a11f",
+    "url": "/portfolio/static/media/team_paypal.7fd1a7a7.png"
+  },
+  {
+    "revision": "8af4bd89141804251baae4d4c218a1f0",
+    "url": "/portfolio/static/media/team_profile.8af4bd89.png"
+  },
+  {
+    "revision": "d3c3f7956383ef8a5050151c3f9354eb",
+    "url": "/portfolio/static/media/wom_admin.d3c3f795.png"
+  },
+  {
+    "revision": "ef080def9d8fd163d5b249e49774f4d4",
+    "url": "/portfolio/static/media/wom_campaign.ef080def.png"
+  },
+  {
+    "revision": "ca07320109737f142c7e2c5ba7bea17a",
+    "url": "/portfolio/static/media/wom_discover.ca073201.png"
+  },
+  {
+    "revision": "778b991225730ea68bbbf6d1df5d8a5a",
+    "url": "/portfolio/static/media/wom_login.778b9912.png"
+  },
+  {
+    "revision": "f881a0d4e9a8df76107098da3fb920fd",
+    "url": "/portfolio/static/media/디테일1.f881a0d4.png"
+  },
+  {
+    "revision": "582a90f3c4f9154e04beef2dc4629a66",
+    "url": "/portfolio/static/media/디테일2.582a90f3.png"
+  },
+  {
+    "revision": "484379869600a189b602bd1dd8d58b8f",
+    "url": "/portfolio/static/media/디테일반응.48437986.png"
+  },
+  {
+    "revision": "a38988c7dd0465134daf3ec53f1130e5",
+    "url": "/portfolio/static/media/디테일반응2.a38988c7.png"
+  },
+  {
+    "revision": "58b7cfce73182e3e1ad7e29d7524af19",
+    "url": "/portfolio/static/media/디테일반응3.58b7cfce.png"
+  },
+  {
+    "revision": "5dd4f15d89e395aa1ef0245bf1c6e3f1",
+    "url": "/portfolio/static/media/망플메인.5dd4f15d.png"
+  },
+  {
+    "revision": "ad332428ab71f7187efb46524bda88f8",
+    "url": "/portfolio/static/media/매거진.ad332428.png"
+  },
+  {
+    "revision": "4e0461c6440b186dfbb753c1571b7c3c",
+    "url": "/portfolio/static/media/매거진반응.4e0461c6.png"
+  },
+  {
+    "revision": "32ee5d42904694e41bc2395651d8b8ba",
+    "url": "/portfolio/static/media/메인반응.32ee5d42.png"
+  },
+  {
+    "revision": "788caf3e398a9b697f2e77d1dadedc3e",
+    "url": "/portfolio/static/media/메인반응2.788caf3e.png"
+  },
+  {
+    "revision": "ffdbbb76ccd3e57829ebc02374ab4ac3",
+    "url": "/portfolio/static/media/부킹.ffdbbb76.png"
+  },
+  {
+    "revision": "edbf15494d7a7467de2050e29734e23e",
+    "url": "/portfolio/static/media/부킹반응.edbf1549.png"
+  },
+  {
+    "revision": "4b818bd8cf07016a75a594d05ce85dfc",
+    "url": "/portfolio/static/media/어바웃.4b818bd8.png"
+  },
+  {
+    "revision": "d99589f61540e37eab4ec696db35125e",
+    "url": "/portfolio/static/media/픽디테일.d99589f6.png"
+  },
+  {
+    "revision": "da8bd05d204f066cadbfa2f2a8174af9",
+    "url": "/portfolio/static/media/픽페이지.da8bd05d.png"
+  },
+  {
+    "revision": "abc9781e6bde10adde5da11393d19236",
+    "url": "/portfolio/static/media/픽페이지반응.abc9781e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolios/precache-manifest.eae9f7ad054ef4c6f373c6cf4060315e.js"
+  "/portfolio/precache-manifest.c3f6cd07082743219365516850cf9f48.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolios/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
