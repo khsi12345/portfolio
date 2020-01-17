29580dc33ee502c868fcd87e95a0fddef6801cad
khsi12345 <khsi6237@gmail.com>
2020-01-17 10:12:00 +0900

    wip

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,7 @@ const Routes = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/portfolio" component={Home}></Route>
         <Route exact path="/projects" component={Projects}></Route>
         <Route exact path="/projects/weplate" component={Weplate}></Route>
         <Route exact path="/projects/we-r-bnb" component={Werbnb}></Route>
